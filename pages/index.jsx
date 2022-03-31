a0de03c96a624b604877d6cc885c17cc8a1445fa
liyisheng <liyisheng@kaikeba.com>
2022-03-31 12:01:43 +0800

    feat: 增加代码

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,27 +1,51 @@
-import { useState } from 'react'
-function Header ({ title }) {
-  return <h1>{title ? title : 'Default title'}</h1>
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+
+import Link from 'next/link'
+import Date from '../components/date'
+
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://www.nextjs.cn/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+            <Link href={`/posts/${id}`}>
+              <a>{title}</a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
+          </li>
+          
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
 }
 
-export default function HomePage () {
-  const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton']
-
-  const [likes, setLikes] = useState(0)
-
-  function handleClick () {
-    setLikes(likes + 1)
-  }
-
-  return (
-    <div>
-      <Header title='Develop. Preview. Ship. ' />
-      <ul>
-        {names.map(name => (
-          <li key={name}>{name}</li>
-        ))}
-      </ul>
-
-      <button onClick={handleClick}>Like ({likes})</button>
-    </div>
-  )
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
